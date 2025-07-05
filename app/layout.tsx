2931b50f467f676140f4d8fbf3cd610e0351b6e5
ToriRuban <toriruban@icloud.com>
2025-07-05 22:18:42 +0300

    feat: create Open Graph metadata for main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,27 @@
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import './global.css';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import React from 'react';
+import { Metadata } from 'next';
+import './global.css';
 
+export const metadata: Metadata = {
+    title: 'NoteHub',
+    description: 'Creating, searching, and deleting notes',
+    openGraph: {
+        title: 'NoteHub',
+        description: 'NoteHub - creating, searching, and deleting notes',
+        url: 'https://08-zustand-opal.vercel.app',
+        images: [
+            {
+                url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+                width: 1200,
+                height: 630,
+                alt: 'NoteHub'
+            }
+        ]
+    }
+};
 export default function RootLayout({
     children,
     modal}: Readonly<{ 
