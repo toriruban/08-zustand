b94113a8b39f4dd2962b8065b8fd82046f7a8806
ToriRuban <toriruban@icloud.com>
2025-07-05 23:57:09 +0300

    feat: add dynamic metadata and Open Graph for filtered notes page

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,11 +1,36 @@
 import NotesClient from './Notes.client';
 import { fetchNotes } from '@/lib/api';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { Metadata } from 'next';
+export async function generateMetadata({ params }: NotesProps): Promise<Metadata> {
+    const { slug } = await params;
+    const isAll = slug[0] === 'All' || slug.length === 0;
+    const tag = isAll ? 'All notes' : slug.join('/');
+    const pageTitle = isAll ? 'All notes | NoteHub' : `Tag: ${tag} | NoteHub`;
+    const pageDescription = isAll ? 'View all notes' : `View notes filtered by: ${tag}`;
+    const filterUrl = `'https://08-zustand-opal.vercel.app/notes/filter/${slug.join(`/`)}`
+    
+    return {
+        title: pageTitle,
+        description: pageDescription,
+        openGraph: {
+            title: pageTitle,
+            description: pageDescription,
+            url: filterUrl,
+            images: [
+            {
+                url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+                width: 1200,
+                height: 630,
+                alt: 'NoteHub'
+            }
+        ]}
+    }
+}
 
 type NotesProps = {
     params: Promise<{ slug: string[] }>
 }
-
 const Notes = async({ params }: NotesProps) => {
     const { slug } = await params;
     const queryClient = new QueryClient();
@@ -17,5 +42,4 @@ const Notes = async({ params }: NotesProps) => {
         </HydrationBoundary>
     )
 }
-
 export default Notes;
